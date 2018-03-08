119bd52f3bdb9002a57d3c960b5d57d90bd9f674
Tomas Koutsky <tomas@stepnivlk.net>
2018-03-08 02:14:27 +0100

    Copy headers to response

--- web.js
+++ web.js
@@ -4,23 +4,37 @@ var app = express();
 var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var port = process.env.PORT || 5000;
 
+function addHeaders(request, response) {
+  var headers = request.headers;
+
+  Object.keys(headers).forEach(key => {
+    var value = headers[key]
+    response.header(key, value)
+  });
+};
+
 app.get('*', function(request, response) {
+  addHeaders(request, response)
   response.redirect(newBaseURL + request.url);
 });
 
 app.post('*', function(request, response) {
+  addHeaders(request, response)
   response.redirect(newBaseURL + request.url);
 });
 
 app.patch('*', function(request, response) {
+  addHeaders(request, response)
   response.redirect(newBaseURL + request.url);
 });
 
 app.put('*', function(request, response) {
+  addHeaders(request, response)
   response.redirect(newBaseURL + request.url);
 });
 
 app.delete('*', function(request, response) {
+  addHeaders(request, response)
   response.redirect(newBaseURL + request.url);
 });
 
